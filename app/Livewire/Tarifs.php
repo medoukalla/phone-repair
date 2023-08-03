d08b10ba74b21f1c60813e85d17d9a1656f63b5f
medoukalla <medoukalla@gmail.com>
2023-08-03 17:31:24 +0100

    Update nos tarifs page

--- app/Livewire/Tarifs.php
+++ app/Livewire/Tarifs.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class Tarifs extends Component
 {
+
+    // get categories 
+    public $categories;
+    
+
     public function render()
     {
+        $this->categories = Category::get_active_categories();
+        
         return view('livewire.tarifs');
     }
 }
--- resources/views/livewire/tarifs.blade.php
+++ resources/views/livewire/tarifs.blade.php
@@ -1,3 +1,37 @@
 <div>
-    {{-- Be like water. --}}
-</div>
+    <div class="hero mb-0">
+      <div class="container">
+        <h1>Réparez vos appareils électroniques en toute confiance.</h1>
+        <div class="companies-board">
+          <h1>Réparation Smartphones, Tablettes et Ordinateurs.</h1>
+          <div class="company">
+  
+            
+            @foreach ($categories as $category)
+    
+              @if ( $category->name == 'Apple' )    
+                <a href="{{ route('frontend.category', $category) }}" wire:navigate>
+                  <div class="apple">
+                    <img src="{{ asset('img/Apple.svg') }}" alt="android" />
+                  </div>
+                </a>
+                
+              @elseif ( $category->name == 'Android' )
+        
+                <a href="{{ route('frontend.category', $category) }}" wire:navigate >
+                  <div class="android">
+                    <img src="{{ asset('img/android.svg') }}" alt="android" />
+                  </div>
+                </a>
+  
+              @endif
+    
+            @endforeach
+    
+          </div>
+        </div>
+      </div>
+    </div>
+   <hr><br><br>
+
+  </div>
